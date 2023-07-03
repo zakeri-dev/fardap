2d5020626417287758103b991c8fead21f3134ce
Mahmoodreza Zakeri <m.zakeri@jaguars.dev>
2023-07-03 11:54:47 +0330

    ok

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
 import { Inter } from "next/font/google";
 import { useEffect, useState } from "react";
-import axios from "axios";
+import axios from 'axios'
 import { getRandomAlphabet } from "../utils/helper";
-// import watermark from "watermarkjs";
 
 export default function Home() {
   const [logo, setLogo] = useState([]);
-  const [nnn, setNnn] = useState([]);
   const [blackWight, setBlackWight] = useState(false);
 
   const fetchLogo = async () => {
@@ -33,52 +31,17 @@ export default function Home() {
 
   useEffect(() => {
     fetchLogo();
-    var ctx = document.createElement("canvas").getContext("2d");
-    var img = new Image();
-    img.onload = () => {
-      ctx.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight);
-      ctx.fillText("COPYRIGHT", 10, 30);
-    };
-    setNnn(img)
   }, []);
-  console.log(nnn)
-
-  // const applyWatermark = () => {
-  //   const image = document.getElementById("image"); // Get the image element
-  //   watermark([image]) // Pass the image element as an array
-  //     .image(
-  //       watermark.text.lowerRight(
-  //         "Your Watermark Text",
-  //         "48px Arial",
-  //         "#fff",
-  //         0.5
-  //       )
-  //     ) // Set the watermark text, font, color, and opacity
-  //     .then((img) => {
-  //       // Replace the original image with the watermarked image
-  //       image.src = img.src;
-  //     });
-  // };
 
   return (
     <main className="flex flex-col min-h-screen items-center justify-center gap-3">
-      {/* <div id="lower-right"></div> */}
-      <img
-        id="image"
-        src='{img.src}'
-        alt=""
-        className={`w-80 ${blackWight ? "grayscale" : null}`}
-      />
+      <img src={logo?.image} alt="" className={`w-80 ${blackWight? 'grayscale' : null}`} />
       <button className="px-3 py-1 bg-cyan-500 rounded-md" onClick={fetchLogo}>
         reload
       </button>
-      <button
-        className="px-3 py-1 bg-cyan-500 rounded-md"
-        onClick={() => setBlackWight(!blackWight)}
-      >
+      <button className="px-3 py-1 bg-cyan-500 rounded-md" onClick={()=>setBlackWight(!blackWight)}>
         Blak & Wight
       </button>
-      {/* <button onClick={applyWatermark}>Apply Watermark</button> */}
     </main>
   );
 }
